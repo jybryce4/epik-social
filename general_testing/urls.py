84338bde93e330931ca2a0b95afeb76087d3716a
jybryce4 <jybryce4@gmail.com>
2020-05-31 00:30:43 -0500

    i think i fixed the image upload problem

--- general_testing/urls.py
+++ general_testing/urls.py
@@ -35,5 +35,5 @@ urlpatterns = [
     path('', include('blog.urls')), # home page
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
